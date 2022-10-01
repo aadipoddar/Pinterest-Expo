cec64e033c3a5b95c9a05d54413a5dacc617fd77
Aadi Poddar <83405769+aadipoddar@users.noreply.github.com>
2022-10-01 14:51:50 +0000

    Initialize nHost backend

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
+import { NhostClient, NhostReactProvider } from "@nhost/react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import * as SecureStore from "expo-secure-store";
+
+const nhost = new NhostClient({
+  backendUrl: "https://qmivxeiavptsoaemctkn.nhost.run",
+  clientStorageType: "expo-secure-storage",
+  clientStorage: SecureStore,
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -13,10 +21,12 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <NhostReactProvider nhost={nhost}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </NhostReactProvider>
     );
   }
 }
